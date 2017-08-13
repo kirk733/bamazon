fb2b8739192168fc93b912979ad42865d5360780
Kirk Henderson <kirk733@me.com>
2017-08-12 22:03:25 -0600

    Challenge 1 - Min Req

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
-
+var inquirer = require('inquirer');
+//var productID = 0;
+//var quantityNum = "";
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -14,14 +16,84 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  queryAllProducts();
 });
 
-function queryAllProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
+connection.query("SELECT * FROM products", function(err, res) {
+    console.log( "Id | Product | Price")
+    console.log("----------------------")
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price );
+      console.log(res[i].item_id + " | " + res[i].product_name + " | $" + res[i].price );
     }
     console.log("-----------------------------------");
+    userSelection(res);
+
   });
+
+    // Ask user what which productID /////////////////////////////////////////////
+function userSelection(res){
+   inquirer.prompt([
+{
+  name: "userChoice",
+  type: "list",
+  choices: ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"],
+  message: "What auction would you like to place a bid in?"
+ }
+      ]).then(function(inquirerResponse) {
+          var productID = inquirerResponse.userChoice
+          inquirer.prompt([
+                  {
+                  type: "input",
+                  message: "How many units of the product they would like to buy.?",
+                  name: "userChoice"
+                  },
+              ]).then(function(inquirerResponse) {
+                  var quantityNum = inquirerResponse.userChoice
+                  checkQuantity(productID, quantityNum);
+              })  //end iquirer
+      })  //end iquirer
+}
+
+function checkQuantity(productID, quantityNum){
+
+  var query = "SELECT * FROM products";
+
+  connection.query(query, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+        if (res[i].item_id == productID){
+          if (res[i].stock_quantity >= quantityNum){
+            var prodCost = quantityNum * res[i].price;
+            var quantityNew = res[i].stock_quantity - quantityNum;
+            updateFinalize(productID, quantityNew, prodCost);
+          }
+          else {
+            console.log("Insufficient quantity!")
+          }
+
+        }
+
+    }
+
+  });
+
+
+}
+
+
+function updateFinalize(productID, quantityNew, prodCost){
+  connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+          stock_quantity: quantityNew
+        },
+        {
+          item_id: productID
+        }
+      ],
+      function(err) {
+        if (err) throw err;
+      }
+    );
+    console.log("The total cost of your purchase is $" + prodCost)
+
 }
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -15,13 +15,13 @@ CREATE TABLE products(
 );
 
 -- Creates new rows
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (10, "socks", 2.50, 1000);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (20, "shirts", 10.50, 500);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (30, "shoes", 20.50, 250);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (40, "jeans", 32.50, 400);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (50, "pencils", 1.50, 50);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (60, "paper", 2.50, 100);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (70, "backpacks", 32.50, 10);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (80, "markers", 4.50, 15);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (10, "socks", 2.00, 1000);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (20, "shirts", 10.00, 500);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (30, "shoes", 20.00, 250);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (40, "jeans", 32.00, 400);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (50, "pencils", 1.00, 50);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (60, "paper", 2.00, 100);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (70, "backpacks", 32.00, 10);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (80, "markers", 4.00, 15);
 INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (90, "rulers", 1.00, 10);
-INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (100, "calculater", 20.50, 20);
+INSERT INTO products (item_id, product_name, price, stock_quantity) VALUES (100, "calculater", 20.00, 20);
